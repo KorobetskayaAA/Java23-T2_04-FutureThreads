7f360a0f7bb498a219294e640537197407b47bf5
А.Коробецкая <kornast@yandex.ru>
2024-01-18 19:09:12 +0400

    Atomic

--- src/ru/teamscore/java23/syncthreads/SharedMonitoring.java
+++ src/ru/teamscore/java23/syncthreads/SharedMonitoring.java
@@ -1,5 +1,6 @@
 package ru.teamscore.java23.syncthreads;
 
+import java.lang.invoke.VolatileCallSite;
 import java.math.BigDecimal;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -7,24 +8,22 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class SharedMonitoring implements Runnable {
     Object lock = new Object();
-    int countIn = 0;
-    int countOut = 0;
-    BigDecimal sum = BigDecimal.ZERO;
+    AtomicInteger countIn = new AtomicInteger(0);
+    AtomicInteger countOut = new AtomicInteger(0);
+    AtomicReference<BigDecimal> sum = new AtomicReference<>(BigDecimal.ZERO);
 
-    public synchronized void add(double value) {
-        countIn++;
-        sum = sum.add(BigDecimal.valueOf(value));
+    public void add(double value) {
+        countIn.incrementAndGet();
+        sum.set(sum.get().add(BigDecimal.valueOf(value)));
     }
 
     public boolean remove(double value) {
         BigDecimal val = BigDecimal.valueOf(value);
-        synchronized (lock) {
-            if (sum.compareTo(val) < 0) {
-                return false;
-            }
-            countOut++;
-            sum = sum.subtract(val);
+        if (sum.get().compareTo(val) < 0) {
+            return false;
         }
+        countOut.incrementAndGet();
+        sum.set(sum.get().subtract(val));
         return true;
     }
 
@@ -32,7 +31,7 @@ public class SharedMonitoring implements Runnable {
     public void run() {
         Thread t = Thread.currentThread();
         while (true) {
-            System.out.printf("\u001B[33m%s +%d, -%d, sum=%s\u001B[0m\n", t.getName(), countIn, countOut, sum);
+            System.out.printf("\u001B[33m%s +%d, -%d, sum=%s\u001B[0m\n", t.getName(), countIn.get(), countOut.get(), sum.get());
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
